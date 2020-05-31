cb9bdfdf535ef9e072ba0ad3e72da92772569a54
Prateek Singh <singhprateek089@gmail.com>
2020-05-31 22:39:42 +0530

    React-signup-login

---
+++ src/components/login.component.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+
+export default class Login extends Component {
+    render() {
+        return (
+            <form>
+                <h3>Sign In</h3>
+
+                <div className="form-group">
+                    <label>Email address</label>
+                    <input type="email" className="form-control" placeholder="Enter email" required />
+                </div>
+
+                <div className="form-group">
+                    <label>Password</label>
+                    <input type="password" className="form-control" placeholder="Enter password" required/>
+                </div>
+
+
+                <button type="submit" className="btn btn-primary btn-block">Submit</button>
+            
+            
+            </form>
+        );
+    }
+}
---
+++ src/components/signup.component.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+
+export default class SignUp extends Component {
+    render() {
+        return (
+            <form>
+                <h3>Sign Up</h3>
+
+                <div className="form-group">
+                    <label>First name</label>
+                    <input type="text" className="form-control" placeholder="First name" required />
+                </div>
+
+                <div className="form-group">
+                    <label>Last name</label>
+                    <input type="text" className="form-control" placeholder="Last name" required />
+                </div>
+
+                <div className="form-group">
+                    <label>Email address</label>
+                    <input type="email" className="form-control" placeholder="Enter email" required/>
+                </div>
+
+                <div className="form-group">
+                    <label>Password</label>
+                    <input type="password" className="form-control" placeholder="Enter password" required/>
+                </div>
+
+                <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
+            </form>
+        );
+    }
+}
